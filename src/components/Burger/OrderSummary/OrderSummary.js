005d2d79ba8c3290a89ed806df0da69ba14f9925
Hobinx <binservmail@gmail.com>
2017-11-30 20:53:01 +0800

    improve modal component

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,29 +1,41 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import Aux from '../../../hoc/Aux';
 import Button from '../../UI/Button/Button';
 
-const orderSummary = props => {
-  const ingredientsSummary = Object.keys(props.ingredients).map(igKey => (
-    <li key={igKey}>
-      <span style={{ textTransform: 'captitalize' }}>{igKey}</span>:
-      {props.ingredients[igKey]}
-    </li>
-  ));
+class OrderSummary extends Component {
+  // componentWillUpdate() {
+  //   console.log('[OrderSummary] WillUpdate');
+  // }
 
-  return (
-    <Aux>
-      <h3>Your Order</h3>
-      <p>A delicious burger with the following ingredients:</p>
-      <ul>
-        {ingredientsSummary}
-      </ul>
-      <p><strong>Total Price: {props.price.toFixed(2)}</strong></p>
-      <p>Continue to checkout?</p>
-      <Button btnType="Danger" clicked={props.purchaseCancelled}>CANCEL</Button>
-      <Button btnType="Success" clicked={props.purchaseContinued}>CONTINUE</Button>
-    </Aux>
-  );
-};
+  render() {
+    const ingredientsSummary = Object.keys(this.props.ingredients).map(
+      igKey => (
+        <li key={igKey}>
+          <span style={{ textTransform: 'captitalize' }}>{igKey}</span>:
+          {this.props.ingredients[igKey]}
+        </li>
+      )
+    );
 
-export default orderSummary;
+    return (
+      <Aux>
+        <h3>Your Order</h3>
+        <p>A delicious burger with the following ingredients:</p>
+        <ul>{ingredientsSummary}</ul>
+        <p>
+          <strong>Total Price: {this.props.price.toFixed(2)}</strong>
+        </p>
+        <p>Continue to checkout?</p>
+        <Button btnType="Danger" clicked={this.props.purchaseCancelled}>
+          CANCEL
+        </Button>
+        <Button btnType="Success" clicked={this.props.purchaseContinued}>
+          CONTINUE
+        </Button>
+      </Aux>
+    );
+  }
+}
+
+export default OrderSummary;
