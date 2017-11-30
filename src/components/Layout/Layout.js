7dbaa52aa9e4e236473d0ff88b834fd7c4e4ea01
Hobinx <binservmail@gmail.com>
2017-11-30 19:57:44 +0800

    add backdrop to sidedrawer

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Aux from '../../hoc/Aux';
 import classes from './Layout.css';
 import Toolbar from '../Navigation/Toolbar/Toolbar';
